42e5f5751ec5496055c6f112e0cd1d6a473379ae
lmarchet <lucasmarchetti21@gmail.com>
2024-11-14 13:08:49 +0100

    doxygen comments

--- src/maestro.c
+++ src/maestro.c
@@ -1,5 +1,33 @@
+/**
+  **********************************************
+  *                               __           
+  *    ____ ___  ____ ____  _____/ /__________ 
+  *   / __ `__ \/ __ `/ _ \/ ___/ __/ ___/ __ \
+  *  / / / / / / /_/ /  __(__  ) /_/ /  / /_/ /
+  * /_/ /_/ /_/\__,_/\___/____/\__/_/   \____/ 
+  *                                            
+  **********************************************
+  * @file   maestro.c
+  * @author Lucas Anatole Marchetti
+  * @brief  Maestro is a C library to simplify
+  *         dynamic allocations more easily.
+  **********************************************
+  * @attention
+  *
+  * Copyright (c) 2024 L.A. MARCHETTI
+  * MIT License
+  **********************************************
+  */
+
 #include "../include/maestro.h"
 
+/**
+  * @brief  Create a new maestro instance.
+  * @param  element_size: Size of individual elements (e.g., sizeof(int) or sizeof(<your_custom_typedef>)).
+  * @warning It is good practice to check if the returned pointer is NULL before using it.
+  * @note   This function allocates memory for the maestro structure but not for the data array.
+  * @retval Pointer to the newly created maestro instance, or NULL if allocation fails.
+  */
 maestro* maestro_new(size_t element_size)
 {
     maestro* ptr_maestro = (maestro*)malloc(sizeof(maestro));
@@ -16,6 +44,16 @@ maestro* maestro_new(size_t element_size)
     return ptr_maestro;
 }
 
+/**
+  * @brief  Append a new element to the maestro instance.
+  * @param  ptr_maestro: Pointer to the maestro instance.
+  * @param  value: Pointer to the value to be appended.
+  * @note   This function reallocates memory to accommodate the new element.
+  *         If reallocation fails, the original data remains unchanged.
+  * @note   The function uses memcpy to copy the new element, so the caller
+  *         is responsible for managing any dynamic memory within the element.
+  * @retval None
+  */
 void maestro_append(maestro* ptr_maestro, const void* value) {
     if (ptr_maestro == NULL)
     {
@@ -37,6 +75,13 @@ void maestro_append(maestro* ptr_maestro, const void* value) {
     ptr_maestro->length++;
 }
 
+/**
+  * @brief  Erase a maestro instance and free associated memory.
+  * @param  ptr_maestro: Pointer to the maestro instance to be erased.
+  * @note   This function frees both the data array and the maestro structure itself.
+  * @note   After calling this function, the ptr_maestro should not be used anymore.
+  * @retval None
+  */
 void maestro_erase(maestro *ptr_maestro)
 {
     if (ptr_maestro == NULL)
